a7a65e65f4c9f876779e3956ba5bda8943d28715
Aurelio D. <aureliopagan6@gmail.com>
2025-05-27 03:02:49 +0000

    Fix comparison operator in inherits_from function for clarity

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     inherited (directly or indirectly) from a_class,
     but not if obj is exactly an instance of a_class.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
