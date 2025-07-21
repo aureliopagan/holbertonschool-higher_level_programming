4bf374bb2cb0fe3e2c3f06ebe3f349385f32f99e
aureliopagan <aurepgst@gmail.com>
2025-07-21 13:56:45 -0400

    add blank lines for improved readability

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script adds all command-line integers and prints the sum."""
 
+
 def calculate_sum():
     """Calculates the sum of all integer arguments."""
     from sys import argv
@@ -8,6 +9,7 @@ def calculate_sum():
     for number in argv[1:]:
         result += int(number)
     return result
+
 
 if __name__ == "__main__":
     print(calculate_sum())
