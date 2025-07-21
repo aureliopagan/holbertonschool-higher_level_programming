eb4fb1deac4e61a5734e53d24938fcba3b3b6f65
aureliopagan <aurepgst@gmail.com>
2025-07-21 13:00:56 -0400

    Working on task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_str = ""
+    for letter in str:
+        ascii_val = ord(letter)
+        if 97 <= ascii_val <= 122:  # Check if lowercase
+            uppercase_str += chr(ascii_val - 32)
+        else:
+            uppercase_str += letter
+    print("{}".format(uppercase_str), end="")
+    print()
