3e70a511d31cd70004aa54ad418e87c12fccd66f
aureliopagan <aurepgst@gmail.com>
2025-07-23 10:16:39 -0400

    fixing task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Prints all integers of a list, one per line"""
-    for num in my_list:
-        print("{:d}".format(num))
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
