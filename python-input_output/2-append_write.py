4876932fad3302d7f4a478a5e32c2bb2e4a868e9
Aurelio D. <aureliopagan6@gmail.com>
2025-07-28 13:28:01 +0000

    Working onm task 3 and fixing task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Handles appending text to files."""
 
+
 def append_write(filename="", text=""):
     """Adds text to end of file and returns character count."""
     with open(filename, 'a', encoding='utf-8') as f:
