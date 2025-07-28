eefa2b414dff715fb098a1452286e39a80601701
Aurelio D. <aureliopagan6@gmail.com>
2025-07-28 13:12:55 +0000

    Working on task 0 and updating readme

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Contains function to display file contents."""
+
+def read_file(filename=""):
+    """Prints contents of UTF-8 text file to screen.
+    
+    Uses context manager for automatic file handling.
+    Reads entire file at once for small files.
+    """
+    with open(filename, mode='r', encoding='utf-8') as file_obj:
+        print(file_obj.read(), end='')
