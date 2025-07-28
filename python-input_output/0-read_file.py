6d8a27fae017d03e8c82820d7697e97b616665fd
Aurelio D. <aureliopagan6@gmail.com>
2025-07-28 13:14:21 +0000

    fixing

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Contains function to display file contents."""
 
+
 def read_file(filename=""):
     """Prints contents of UTF-8 text file to screen.
-    
+
     Uses context manager for automatic file handling.
     Reads entire file at once for small files.
     """
