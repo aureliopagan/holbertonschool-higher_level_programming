634e7ce0ab8a7191db605bec54245c57f08cf821
Aurelio D. <aureliopagan6@gmail.com>
2025-07-28 13:58:14 +0000

    Working on task 8 and 9

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Converts class attributes to JSON-serializable dictionary."""
 
+
 def class_to_json(obj):
     """Returns object's attributes as a dictionary.
-    
+
     Gets all attributes that can be serialized to JSON.
     """
     return obj.__dict__
---
+++ python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a student class with JSON conversion capability."""
+
+class Student:
+    """Represents a student with basic info and JSON conversion."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The student's first name
+            last_name (str): The student's last name
+            age (int): The student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of the Student.
+
+        Returns:
+            dict: The student's attributes as key/value pairs
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
