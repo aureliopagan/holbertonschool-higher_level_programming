da69b0ae3b7efbffc88354f8671ce861863990ec
Aurelio D. <aureliopagan6@gmail.com>
2025-07-28 05:52:24 +0000

    Working on tasdk 3 and 4

--- python-more_data_structures/3-main.py
+++ python-more_data_structures/3-main.py
@@ -1,17 +1,7 @@
-# 3-main.py
+#!/usr/bin/python3
+common_elements = __import__('3-common_elements').common_elements
 
-from common_elements import common_elements
-
-# Test cases for common_elements function
-if __name__ == "__main__":
-    list1 = [1, 2, 3, 4, 5]
-    list2 = [4, 5, 6, 7, 8]
-    print("Common elements:", common_elements(list1, list2))
-
-    list1 = ["apple", "banana", "cherry"]
-    list2 = ["banana", "kiwi", "cherry"]
-    print("Common elements:", common_elements(list1, list2))
-
-    list1 = [1, 2, 3]
-    list2 = [4, 5, 6]
-    print("Common elements:", common_elements(list1, list2))
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
--- python-more_data_structures/4-main.py
+++ python-more_data_structures/4-main.py
@@ -1,18 +1,7 @@
-# 4-main.py
-
 #!/usr/bin/python3
-"""
-Main file for testing the function only_diff_elements.
-"""
+only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
 
-from 4-only_diff_elements import only_diff_elements
-
-# Test cases
-if __name__ == "__main__":
-    list_1 = [1, 2, 3, 4, 5]
-    list_2 = [4, 5, 6, 7, 8]
-    print(only_diff_elements(list_1, list_2))  # Expected output: [1, 2, 3, 6, 7, 8]
-
-    list_1 = ["apple", "banana", "cherry"]
-    list_2 = ["banana", "kiwi", "mango"]
-    print(only_diff_elements(list_1, list_2))  # Expected output: ['apple', 'cherry', 'kiwi', 'mango']
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
