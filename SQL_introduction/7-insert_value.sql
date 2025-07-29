09f39d0fac2c2e37feff0f7ee65e3b83eeff2943
aureliopagan <aurepgst@gmail.com>
2025-07-29 09:43:10 -0400

    Working on task 7 and  8

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Add a new record to first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- Inserts a row with id 89 and name 'Best School'
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Count the number of rows in first_table with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+-- Useful for checking if a specific record exists
