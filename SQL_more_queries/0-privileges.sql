f587b3ffe1349d1cf6732dd3b0f899da01edd58f
aureliopagan <aurepgst@gmail.com>
2025-07-29 09:57:28 -0400

    Creating file structure and working on task 0 and updating readme

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Retrieve the granted privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Retrieve the granted privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+-- This provides visibility into the permissions assigned to each user
