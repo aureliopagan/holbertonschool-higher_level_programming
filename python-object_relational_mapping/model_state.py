ae0ac84264d39124bab284c255d779e55d59f21b
aureliopagan <aurepgst@gmail.com>
2025-07-29 11:47:55 -0400

    working on task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,44 +1,25 @@
 #!/usr/bin/python3
-"""Script that links the State class to the `states` table in a database"""
-
-import sys
+"""
+class definition module
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
-# Base class for all models
+
 Base = declarative_base()
 
 
 class State(Base):
-    """State class that maps to the 'states' table in the database."""
+    """State class"""
     __tablename__ = 'states'
-
-    # Column definitions for the 'states' table
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    # Ensure correct number of arguments are passed
-    if len(sys.argv) != 4:
-        print(
-            "Usage: ./6-model_state.py mysql_username "
-            "mysql_password database_name"
+    id = Column(
+        Integer,
+        unique=True,
+        autoincrement=True,
+        primary_key=True,
+        nullable=False
         )
-        sys.exit(1)
-
-    # Extract command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create engine and bind to Base
-    engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}'
-        f'@localhost/{database_name}',
-        pool_pre_ping=True
+    name = Column(
+        String(128),
+        nullable=False
     )
-
-    # Create all tables
-    Base.metadata.create_all(engine)
