8c7a178d1eea3e788512fa8aa5e651ab76e9053f
aureliopagan <aurepgst@gmail.com>
2025-07-29 09:41:09 -0400

    Working on task 5 and 6

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Retrieve the complete table definition
+SHOW CREATE TABLE first_table;
+-- Displays the SQL statement used to create the table
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+SELECT * FROM first_table;
+-- Path: SQL_introduction/6-list_values.sql
