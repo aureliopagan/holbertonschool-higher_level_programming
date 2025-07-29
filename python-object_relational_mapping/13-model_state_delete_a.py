b582ac87367e0a24871fd3375ac448e06162c59a
aureliopagan <aurepgst@gmail.com>
2025-07-29 12:16:34 -0400

    fixing task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa
+Deletes all State objects with a name containing the letter 'a' from the database hbtn_0e_6_usa
 """
+
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
