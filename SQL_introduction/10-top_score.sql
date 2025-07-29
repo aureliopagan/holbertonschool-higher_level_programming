bca6e7b33342f4bd00af6e3785e304ea3ec49b60
aureliopagan <aurepgst@gmail.com>
2025-07-29 09:46:01 -0400

    Working on task 10 and 11

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Retrieve the name and score columns from second_table
+-- Sorted in descending order by the score column
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Select the name and score columns from second_table
+-- Where the score is greater than or equal to 10
+-- Sorted in descending order by the score column
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
