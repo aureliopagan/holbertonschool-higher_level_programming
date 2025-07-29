ca8e83f800f58823e3858c53114a30cbb565971f
aureliopagan <aurepgst@gmail.com>
2025-07-29 09:35:12 -0400

    Working on task 1

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Script: Create a new database if it doesn't exist already
+-- Database name: hbtn_0c_0
+-- Note: This command is idempotent - safe to run multiple times
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
