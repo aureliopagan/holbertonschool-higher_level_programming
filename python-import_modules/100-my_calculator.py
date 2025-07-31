ae6389dd92ced1da7282428af1218cb7b78f2fca
aureliopagan <aurepgst@gmail.com>
2025-07-31 18:30:47 -0400

    updating

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,17 +3,17 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
+
     # Check if the number of arguments is correct
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     # Get arguments
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-    
+
     # Define valid operators and their corresponding functions
     operators = {
         '+': add,
@@ -21,14 +21,14 @@ if __name__ == "__main__":
         '*': mul,
         '/': div
     }
-    
+
     # Check if operator is valid
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
+
     # Perform calculation
     result = operators[operator](a, b)
-    
+
     # Print result
     print("{} {} {} = {}".format(a, operator, b, result))
