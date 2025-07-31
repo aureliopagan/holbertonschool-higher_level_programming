fa9c584fa2e711805a0525cef375716c22aafdba
aureliopagan <aurepgst@gmail.com>
2025-07-31 18:05:13 -0400

    updating 101

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -32,7 +32,8 @@ class Square:
     @position.setter
     def position(self, value):
         """Set the position of the Square."""
-        if (not isinstance(value, tuple) or len(value) != 2 or
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
