282604ed82a27a1afb3b5ee96895b49098066a74
aureliopagan <aurepgst@gmail.com>
2025-07-31 18:26:43 -0400

    updating

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""Program to perform basic arithmetic operations."""
 
-import calculator_1
-import sys
-
-def main():
-    """Main function to handle the calculator logic."""
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    
+    # Check if the number of arguments is correct
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
+    
+    # Get arguments
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-
-    if operator == '+':
-        result = calculator_1.add(a, b)
-    elif operator == '-':
-        result = calculator_1.sub(a, b)
-    elif operator == '*':
-        result = calculator_1.mul(a, b)
-    elif operator == '/':
-        result = calculator_1.div(a, b)
-    else:
+    
+    # Define valid operators and their corresponding functions
+    operators = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+    
+    # Check if operator is valid
+    if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-
-    print(f"{a} {operator} {b} = {result}")
-
-if __name__ == "__main__":
-    main()
+    
+    # Perform calculation
+    result = operators[operator](a, b)
+    
+    # Print result
+    print("{} {} {} = {}".format(a, operator, b, result))
