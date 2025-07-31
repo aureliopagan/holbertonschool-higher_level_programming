9df9c9b917cb96d6f35c3001484dd67d5f2b8ac2
aureliopagan <aurepgst@gmail.com>
2025-07-31 18:46:24 -0400

    advanced

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print("{}".format("".join(chr(122 - i).lower() if i % 2 == 0 else
-                           chr(122 - i).upper() for i in range(26))), end="")
+                          chr(122 - i).upper() for i in range(26))), end="")
